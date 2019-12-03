4217d24f351237d413a0a46682db275282e34292
Augustin Grigorov <a.a.grigorov@gmail.com>
2019-12-03 22:34:38 +0000

    Working 3.1

--- day3.1/main.go
+++ day3.1/main.go
@@ -28,107 +28,79 @@ type bounds struct {
 }
 
 func main() {
-	wireOneLines := make([]line, 0)
-	wireTwoLines := make([]line, 0)
-	gridBounds := bounds{}
+	wireOnePoints := make([]string, 0)
+	wireTwoPoints := make([]string, 0)
 
-	input, err := ioutil.ReadFile("input1.txt")
+	input, err := ioutil.ReadFile("input.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 	inputString := string(input)
 	inputArray := strings.Split(strings.Trim(inputString, "\n"), "\n")
-	//wireOneInput := strings.Split(inputArray[0], ",")
+	wireOneInput := strings.Split(inputArray[0], ",")
 	wireTwoInput := strings.Split(inputArray[1], ",")
+
 	currentCoordinates := coordinates{0, 0}
-	//for _, lineFragmentSpec := range wireOneInput {
-	//	var line line
-	//	line, currentCoordinates = computeLineFragmentsAndBounds(currentCoordinates, lineFragmentSpec, &gridBounds)
-	//	wireOneLines = append(wireOneLines, line)
-	//}
+	for _, lineFragmentSpec := range wireOneInput {
+		var linePointsCoordinateStrings []string
+		linePointsCoordinateStrings, currentCoordinates = computeLineFragments(currentCoordinates, lineFragmentSpec)
+		wireOnePoints = append(wireOnePoints, linePointsCoordinateStrings...)
+	}
+
 	currentCoordinates = coordinates{0, 0}
 	for _, lineFragmentSpec := range wireTwoInput {
-		var line line
-		line, currentCoordinates = computeLineFragmentsAndBounds(currentCoordinates, lineFragmentSpec, &gridBounds)
-		wireTwoLines = append(wireTwoLines, line)
+		var linePointsCoordinateStrings []string
+		linePointsCoordinateStrings, currentCoordinates = computeLineFragments(currentCoordinates, lineFragmentSpec)
+		wireTwoPoints = append(wireTwoPoints, linePointsCoordinateStrings...)
 	}
 
-	fmt.Println(gridBounds)
+	intersections := findIntersections(wireOnePoints, wireTwoPoints)
+	var lowestDistance int
+	for _, intersection := range intersections {
+		if intersection != "0,0" {
+			pointCoordinates := strings.Split(intersection, ",")
+			pointX, _ := strconv.Atoi(pointCoordinates[0])
+			pointY, _ := strconv.Atoi(pointCoordinates[1])
+			distance := abs(pointX) + abs(pointY)
+			if lowestDistance == 0 || lowestDistance > distance {
+				lowestDistance = distance
+			}
+		}
+	}
 
-	grid := make([][]int, abs(gridBounds.x.lowerBound)+gridBounds.x.upperBound)
-	for i := range grid {
-		grid[i] = make([]int, abs(gridBounds.y.lowerBound)+gridBounds.y.upperBound)
-	}
-	fmt.Println("Grid compilation complete")
-	for _, line := range wireOneLines {
-		plotLine(line, grid, gridBounds, 1)
-	}
-	for _, line := range wireTwoLines {
-		plotLine(line, grid, gridBounds, 2)
-	}
+	fmt.Println(lowestDistance)
 }
 
-func computeLineFragmentsAndBounds(currentPosition coordinates, lineFragmentSpec string, gridBounds *bounds) (line, coordinates) {
+func computeLineFragments(currentPosition coordinates, lineFragmentSpec string) ([]string, coordinates) {
 	distance, err := strconv.Atoi(lineFragmentSpec[1:])
-	line := line{
-		beginning: currentPosition,
-	}
+	var linePoints []string
 	var newPosition coordinates
 	if err != nil {
 		fmt.Printf("can't parse distance from %v %v \n", lineFragmentSpec, err)
 	}
 	switch lineFragmentSpec[0] {
 	case 'L':
-		if currentPosition.x-distance < gridBounds.x.lowerBound {
-			gridBounds.x.lowerBound = currentPosition.x - distance
-		}
 		newPosition = coordinates{currentPosition.x - distance, currentPosition.y}
+		for i := 0; i > -distance; i-- {
+			linePoints = append(linePoints, fmt.Sprintf("%v,%v", currentPosition.x+i, currentPosition.y))
+		}
 	case 'R':
-		if currentPosition.x+distance > gridBounds.x.upperBound {
-			gridBounds.x.upperBound = currentPosition.x + distance
-		}
 		newPosition = coordinates{currentPosition.x + distance, currentPosition.y}
+		for i := 0; i < distance; i++ {
+			linePoints = append(linePoints, fmt.Sprintf("%v,%v", currentPosition.x+i, currentPosition.y))
+		}
 	case 'U':
-		if currentPosition.y+distance > gridBounds.y.upperBound {
-			gridBounds.y.upperBound = currentPosition.y + distance
-		}
 		newPosition = coordinates{currentPosition.x, currentPosition.y + distance}
+		for i := 0; i < distance; i++ {
+			linePoints = append(linePoints, fmt.Sprintf("%v,%v", currentPosition.x, currentPosition.y+i))
+		}
 	case 'D':
-		if currentPosition.y-distance < gridBounds.y.lowerBound {
-			gridBounds.y.lowerBound = currentPosition.y - distance
-		}
 		newPosition = coordinates{currentPosition.x, currentPosition.y - distance}
-	}
-	line.end = newPosition
-	return line, newPosition
-}
-
-func plotLine(line line, grid [][]int, gridBounds bounds, lineNumber int) {
-	xOffset := abs(gridBounds.x.lowerBound)
-	yOffset := abs(gridBounds.y.lowerBound)
-
-	if line.beginning.x == line.end.x {
-		if line.end.y-line.beginning.y < 0 {
-			for i := 0; i > line.end.y-line.beginning.y; i-- {
-				grid[line.beginning.x+xOffset][line.beginning.y+yOffset+i] = lineNumber
-			}
-		} else {
-			for i := 0; i < line.end.y-line.beginning.y; i++ {
-				grid[line.beginning.x+xOffset][line.beginning.y+yOffset+i] = lineNumber
-			}
-		}
-	} else {
-		if line.end.x-line.beginning.x < 0 {
-			for i := 0; i > line.beginning.x-line.end.x; i-- {
-				grid[line.beginning.x+xOffset+i][line.beginning.y+yOffset] = lineNumber
-			}
-		} else {
-			for i := 0; i < line.end.x-line.beginning.x; i++ {
-				fmt.Println(line, xOffset, gridBounds.x.upperBound)
-				grid[line.beginning.x+xOffset+i][line.beginning.y+yOffset] = lineNumber
-			}
+		for i := 0; i > -distance; i-- {
+			linePoints = append(linePoints, fmt.Sprintf("%v,%v", currentPosition.x, currentPosition.y+i))
 		}
 	}
+	return linePoints, newPosition
 }
 
 func abs(x int) int {
@@ -136,4 +108,19 @@ func abs(x int) int {
 		return -x
 	}
 	return x
+}
+
+func findIntersections(a, b []string) (c []string) {
+	m := make(map[string]bool)
+
+	for _, item := range a {
+		m[item] = true
+	}
+
+	for _, item := range b {
+		if _, ok := m[item]; ok {
+			c = append(c, item)
+		}
+	}
+	return
 }
