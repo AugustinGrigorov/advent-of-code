196fcb5ff6cd442bc9e9f1d75fd1160f8d17b8c6
AugustinGrigorov <a.a.grigorov@gmail.com>
2019-12-05 14:08:18 +0000

    Day 5.2 ⭐️

---
+++ day5.2/main.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+const input = 5
+
+func main() {
+	input, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	inputString := string(input)
+	inputArray := strings.Split(strings.Trim(inputString, "\n"), ",")
+	outputArray := make([]int, len(inputArray))
+	for index, opcode := range inputArray {
+		outputArray[index], err = strconv.Atoi(opcode)
+		if err != nil {
+			panic(err)
+		}
+	}
+	executeIndex(0, outputArray)
+}
+
+func executeIndex(index int, array []int) []int {
+	operator := array[index]
+	var param1, param2 int
+
+	switch operator % 100 {
+	case 1:
+		firstParamImmediateMode, secondParamImmediateMode, thirdParamImmediateMode := getFlags(array[index])
+		if firstParamImmediateMode {
+			param1 = array[index+1]
+		} else {
+			param1 = array[array[index+1]]
+		}
+		if secondParamImmediateMode {
+			param2 = array[index+2]
+		} else {
+			param2 = array[array[index+2]]
+		}
+		if thirdParamImmediateMode {
+			array[index+3] = param1 + param2
+		} else {
+			array[array[index+3]] = param1 + param2
+		}
+		return executeIndex(index+4, array)
+	case 2:
+		firstParamImmediateMode, secondParamImmediateMode, thirdParamImmediateMode := getFlags(array[index])
+		if firstParamImmediateMode {
+			param1 = array[index+1]
+		} else {
+			param1 = array[array[index+1]]
+		}
+		if secondParamImmediateMode {
+			param2 = array[index+2]
+		} else {
+			param2 = array[array[index+2]]
+		}
+		if thirdParamImmediateMode {
+			array[index+3] = param1 * param2
+		} else {
+			array[array[index+3]] = param1 * param2
+		}
+		return executeIndex(index+4, array)
+	case 3:
+		firstParamImmediateMode, _, _ := getFlags(array[index])
+		if firstParamImmediateMode {
+			array[index+1] = input
+		} else {
+			array[array[index+1]] = input
+		}
+		return executeIndex(index+2, array)
+	case 4:
+		firstParamImmediateMode, _, _ := getFlags(array[index])
+		if firstParamImmediateMode {
+			param1 = array[index+1]
+		} else {
+			param1 = array[array[index+1]]
+		}
+		fmt.Println(param1)
+		return executeIndex(index+2, array)
+	case 5:
+		firstParamImmediateMode, secondParamImmediateMode, _ := getFlags(array[index])
+		if firstParamImmediateMode {
+			param1 = array[index+1]
+		} else {
+			param1 = array[array[index+1]]
+		}
+		if secondParamImmediateMode {
+			param2 = array[index+2]
+		} else {
+			param2 = array[array[index+2]]
+		}
+		if param1 > 0 {
+			return executeIndex(param2, array)
+		}
+		return executeIndex(index+3, array)
+	case 6:
+		firstParamImmediateMode, secondParamImmediateMode, _ := getFlags(array[index])
+		if firstParamImmediateMode {
+			param1 = array[index+1]
+		} else {
+			param1 = array[array[index+1]]
+		}
+		if secondParamImmediateMode {
+			param2 = array[index+2]
+		} else {
+			param2 = array[array[index+2]]
+		}
+		if param1 == 0 {
+			return executeIndex(param2, array)
+		}
+		return executeIndex(index+3, array)
+	case 7:
+		firstParamImmediateMode, secondParamImmediateMode, thirdParamImmediateMode := getFlags(array[index])
+		if firstParamImmediateMode {
+			param1 = array[index+1]
+		} else {
+			param1 = array[array[index+1]]
+		}
+		if secondParamImmediateMode {
+			param2 = array[index+2]
+		} else {
+			param2 = array[array[index+2]]
+		}
+		var value int
+		if param1 < param2 {
+			value = 1
+		}
+		if thirdParamImmediateMode {
+			array[index+3] = value
+		} else {
+			array[array[index+3]] = value
+		}
+		return executeIndex(index+4, array)
+	case 8:
+		firstParamImmediateMode, secondParamImmediateMode, thirdParamImmediateMode := getFlags(array[index])
+		if firstParamImmediateMode {
+			param1 = array[index+1]
+		} else {
+			param1 = array[array[index+1]]
+		}
+		if secondParamImmediateMode {
+			param2 = array[index+2]
+		} else {
+			param2 = array[array[index+2]]
+		}
+		var value int
+		if param1 == param2 {
+			value = 1
+		}
+		if thirdParamImmediateMode {
+			array[index+3] = value
+		} else {
+			array[array[index+3]] = value
+		}
+		return executeIndex(index+4, array)
+	case 99:
+		return array
+	}
+	return array
+}
+
+func getFlags(operator int) (firstParamImmediateMode bool, secondParamImmediateMode bool, thirdParamImmediateMode bool) {
+	if operator > 10000 {
+		thirdParamImmediateMode = true
+		if operator/1000%10 == 1 {
+			secondParamImmediateMode = true
+		}
+		if operator/100%10 == 1 {
+			firstParamImmediateMode = true
+		}
+		return
+	}
+	if operator > 1000 {
+		secondParamImmediateMode = true
+		if operator/100%10 == 1 {
+			firstParamImmediateMode = true
+		}
+		return
+	}
+	if operator > 100 {
+		firstParamImmediateMode = true
+		return
+	}
+	return false, false, false
+}
