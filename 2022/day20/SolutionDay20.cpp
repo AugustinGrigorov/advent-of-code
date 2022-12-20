20a1c97ff9ab4f5f3729165a5315ea9a364afd87
Augustin Grigorov <grigoroa@amazon.com>
2022-12-20 19:49:17 +0000

    Optimisation

--- 2022/day20/SolutionDay20.cpp
+++ 2022/day20/SolutionDay20.cpp
@@ -10,23 +10,23 @@ struct Node {
 public:
     Node(int64_t value): value(value){};
     int64_t value;
-    std::shared_ptr<Node> left;
-    std::shared_ptr<Node> right;
+    Node * left;
+    Node * right;
 };
 
-std::vector<std::shared_ptr<Node>> getListOrder(const std::shared_ptr<Node>& root){
-    std::vector<std::shared_ptr<Node>> order = {};
-    order.push_back(root);
-    std::shared_ptr<Node> current;
-    current = root->right;
-    while (current != root) {
-        order.push_back(current);
-        current = current->right;
-    }
-    return order;
-}
+//std::vector<std::shared_ptr<Node>> getListOrder(const std::shared_ptr<Node>& root){
+//    std::vector<std::shared_ptr<Node>> order = {};
+//    order.push_back(root);
+//    std::shared_ptr<Node> current;
+//    current = root->right;
+//    while (current != root) {
+//        order.push_back(current);
+//        current = current->right;
+//    }
+//    return order;
+//}
 
-void runCycles(const std::vector<std::shared_ptr<Node>>& order, int numberOfCycles) {
+void runCycles(const std::vector<std::unique_ptr<Node>>& order, int numberOfCycles) {
     for (int i = 0; i < numberOfCycles; ++i) {
         for (const auto& node: order) {
             if(node->value == 0) {
@@ -34,17 +34,18 @@ void runCycles(const std::vector<std::shared_ptr<Node>>& order, int numberOfCycl
             }
             auto oldLeftNeighbour = node->left;
             auto oldRightNeighbour = node->right;
-            std::shared_ptr<Node> newLeftNeighbour = {};
-            std::shared_ptr<Node> newRightNeighbour = {};
+            Node * newLeftNeighbour = {};
+            Node * newRightNeighbour = {};
             oldLeftNeighbour->right = oldRightNeighbour;
             oldRightNeighbour->left = oldLeftNeighbour;
 
             if (node->value > 0) {
-                newLeftNeighbour = node;
+                newLeftNeighbour = node.get();
             } else if (node->value < 0) {
-                newRightNeighbour = node;
+                newRightNeighbour = node.get();
             }
-            for (size_t j = 0; j < abs(node->value) % (order.size() - 1); ++j) {
+            auto moveSize = abs(node->value) % (order.size() - 1);
+            for (size_t j = 0; j < moveSize; ++j) {
                 if (node->value > 0) {
                     newLeftNeighbour = newLeftNeighbour->right;
                 } else if (node->value < 0) {
@@ -60,31 +61,34 @@ void runCycles(const std::vector<std::shared_ptr<Node>>& order, int numberOfCycl
                 newLeftNeighbour = newRightNeighbour->left;
             }
 
-            newLeftNeighbour->right = node;
-            newRightNeighbour->left = node;
+            newLeftNeighbour->right = node.get();
+            newRightNeighbour->left = node.get();
             node->left = newLeftNeighbour;
             node->right = newRightNeighbour;
         }
     }
 }
 
-std::pair<std::shared_ptr<Node>, std::shared_ptr<Node>> generateNodes(std::vector<std::string> &lines, int64_t multiplier){
-    std::shared_ptr<Node> root = std::make_shared<Node>(atoi(lines[0].c_str()) * multiplier);
-    std::shared_ptr<Node> current = root;
-    std::shared_ptr<Node> nodeZero;
+std::pair<std::vector<std::unique_ptr<Node>>, Node *> generateNodes(std::vector<std::string> &lines, int64_t multiplier){
+    std::vector<std::unique_ptr<Node>> order = {};
+    std::unique_ptr<Node> root = std::make_unique<Node>(atoi(lines[0].c_str()) * multiplier);
+    Node * current = root.get();
+    order.push_back(std::move(root));
+    Node * nodeZero=nullptr;
     for (size_t i = 1; i < lines.size(); ++i) {
         auto &line = lines[i];
-        std::shared_ptr<Node> newNode = std::make_shared<Node>(atoi(line.c_str()) * multiplier);
+        auto newNode = std::make_unique<Node>(atoi(line.c_str()) * multiplier);
         if (newNode->value == 0) {
-            nodeZero = newNode;
+            nodeZero = newNode.get();
         }
-        current->right = newNode;
+        current->right = newNode.get();
         newNode->left = current;
-        current = newNode;
+        current = newNode.get();
+        order.push_back(std::move(newNode));
     }
-    current->right = root;
-    root->left = current;
-    return std::make_pair(root, nodeZero);
+    current->right = root.get();
+    order[0]->left = current;
+    return std::make_pair(std::move(order), nodeZero);
 }
 
 SolutionDay20::SolutionDay20(std::ifstream input) {
@@ -93,12 +97,12 @@ SolutionDay20::SolutionDay20(std::ifstream input) {
         inputLines.push_back(line);
     }
 
-    auto [rootPart1, nodeZeroPart1] = generateNodes(inputLines, 1);
-    runCycles(getListOrder(rootPart1), 1);
+    auto [orderPart1, nodeZeroPart1] = generateNodes(inputLines, 1);
+    runCycles(orderPart1, 1);
     auto currentPart1 = nodeZeroPart1;
 
-    auto [rootPart2, nodeZeroPart2] = generateNodes(inputLines, 811589153);
-    runCycles(getListOrder(rootPart2), 10);
+    auto [orderPart2, nodeZeroPart2] = generateNodes(inputLines, 811589153);
+    runCycles(orderPart2, 10);
     auto currentPart2 = nodeZeroPart2;
 
     for (int i = 0; i < 3; ++i) {
